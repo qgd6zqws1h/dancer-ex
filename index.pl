5880c63e12ba8b4f8e9affa85e87737da012d694
Geir Helland <geir.helland@kantega.no>
2018-11-13 10:11:07 +0100

    -

--- index.pl
+++ index.pl
@@ -9,6 +9,15 @@ use Plack::Runner;
 set apphandler => 'PSGI';
 set environment => 'production';
 
+# Legger på sikkerhets-headere med fornuftige defaultverdier:
+set header 'X-XSS-Protection' => "1; 'mode=block'";
+set header 'X-Download-Options' => 'noopen';
+set header 'X-Content-Type-Options' => 'nosniff';
+set header 'Strict-Transport-Security' => 'max-age=3600';
+set header 'X-Frame-Options' => 'DENY';
+set header 'Server' => 'nginx';
+set header 'Content-Security-Policy' =>  "default-src 'self'; font-src *;img-src * data:; script-src *; style-src *;";
+
 my $psgi;
 $psgi = path($ENV{'DOCUMENT_ROOT'}, 'bin', 'app.psgi');
 die "Unable to read startup script: $psgi" unless -r $psgi;
--- lib/inventory.pm
+++ lib/inventory.pm
@@ -19,28 +19,25 @@ sub get_connection{
 	$ENV{'MYSQL_PASSWORD'}='dance-dance-revolution';
     }
 
-  my $service_name=uc $ENV{'DATABASE_SERVICE_NAME'};
-  my $db_host=$ENV{"${service_name}_SERVICE_HOST"};
-  my $db_port=$ENV{"${service_name}_SERVICE_PORT"};
-  my $dbh=DBI->connect("DBI:mysql:database=$ENV{'MYSQL_DATABASE'};host=$db_host;port=$db_port",$ENV{'MYSQL_USER'},$ENV{'MYSQL_PASSWORD'}, { RaiseError => 1 } ) or die ("Couldn't connect to database: " . DBI->errstr );
-  return $dbh;
+    my $service_name=uc $ENV{'DATABASE_SERVICE_NAME'};
+    my $db_host=$ENV{"${service_name}_SERVICE_HOST"};
+    my $db_port=$ENV{"${service_name}_SERVICE_PORT"};
+    my $dbh=DBI->connect("DBI:mysql:database=$ENV{'MYSQL_DATABASE'};host=$db_host;port=$db_port",$ENV{'MYSQL_USER'},$ENV{'MYSQL_PASSWORD'}, { RaiseError => 1 } ) or die ("Couldn't connect to database: " . DBI->errstr );
+    return $dbh;
 }
 
 sub init_db{
-
-  my $dbh = $_[0];
-
-  eval { $dbh->do("DROP TABLE foo") };
-
-  $dbh->do("CREATE TABLE foo (id INTEGER not null auto_increment, name VARCHAR(20), email VARCHAR(30), PRIMARY KEY(id))");
-  $dbh->do("INSERT INTO foo (name, email) VALUES (" . $dbh->quote("Eric Cartman") . ", " . $dbh->quote("Cartman\@SouthPark.com") . ")");
-  $dbh->do("INSERT INTO foo (name, email) VALUES (" . $dbh->quote("Stan March") . ", " . $dbh->quote("March\@SouthPark.com") . ")");
-  $dbh->do("INSERT INTO foo (name, email) VALUES (" . $dbh->quote("Kyle Broflovski") . ", " . $dbh->quote("Broflovski\@SouthPark.com") . ")");
-  $dbh->do("INSERT INTO foo (name, email) VALUES (" . $dbh->quote("Kenny McCormick") . ", " . $dbh->quote("Kenny\@SouthPark.com") . ")");
-
+    my $dbh = $_[0];
+    eval { $dbh->do("DROP TABLE foo") };
+    $dbh->do("CREATE TABLE foo (id INTEGER not null auto_increment, name VARCHAR(20), email VARCHAR(30), PRIMARY KEY(id))");
+    $dbh->do("INSERT INTO foo (name, email) VALUES (" . $dbh->quote("Eric Cartman") . ", " . $dbh->quote("Cartman\@SouthPark.com") . ")");
+    $dbh->do("INSERT INTO foo (name, email) VALUES (" . $dbh->quote("Stan March") . ", " . $dbh->quote("March\@SouthPark.com") . ")");
+    $dbh->do("INSERT INTO foo (name, email) VALUES (" . $dbh->quote("Kyle Broflovski") . ", " . $dbh->quote("Broflovski\@SouthPark.com") . ")");
+    $dbh->do("INSERT INTO foo (name, email) VALUES (" . $dbh->quote("Kenny McCormick") . ", " . $dbh->quote("Kenny\@SouthPark.com") . ")");
 };
 
 get '/user/:id' => sub {
+    addSecurityHeaders();
     my $timestamp = localtime();
     my $dbh = get_connection();
 
@@ -52,9 +49,42 @@ get '/user/:id' => sub {
 
     template user => {timestamp => $timestamp, data => $data};
 };
+sub addSecurityHeaders() {
+    set header 'X-XSS-Protection' => "1; 'mode=block'";
+    set header 'X-Download-Options' => 'noopen';
+    set header 'X-Content-Type-Options' => 'nosniff';
+    set header 'Strict-Transport-Security' => 'max-age=3600';
+    set header 'X-Frame-Options' => 'DENY';
+    #set header 'Server' => 'nginx';
+    set header 'Content-Security-Policy' =>
+	"default-src 'self'; font-src *;img-src * data:; script-src *; style-src *";
+}
+get '/headers' => sub {
+    # Legger på sikkerhets-headere med fornuftige defaultverdier:
+    addSecurityHeaders();
+    my $hrefReq=request->params;
+    my $headerObj=request->headers();
+    my $headerNames="";
+    if (defined $headerObj && $headerObj ) {
+	#$headerObj->remove_header('Server');
+	foreach ($headerObj->header_field_names()) {
+	    $headerNames.=sprintf("%-42s: %s\n","$_",$headerObj->header("$_"));
+	}
+    }
+    my $htmlOutPut="<h3>headers</h3>\n<pre>\n$headerNames\n<hr>";
+    $htmlOutPut.="<h3>Parameters</h3><pre>\n";
+    foreach (sort keys %{$hrefReq} ) {
+	$htmlOutPut.=sprintf("%-42s: %s\n","$_","$$hrefReq{$_}");
+    }
+#    $htmlOutPut.="Request URI: $requestUri\n";
+#    $htmlOutPut.="URI base: $uriBase\n";
+    $htmlOutPut.="\n</pre>\n";
+    return $htmlOutPut;
+};
 
 get '/env' => sub {
-    my $htmlOutPut="<pre>\n";
+    addSecurityHeaders();
+    my $htmlOutPut="<h3>Env</h3>\n<pre>\n";
     foreach (sort keys %ENV ) {
 	$htmlOutPut.=sprintf("%-42s: %s\n","$_","$ENV{$_}");
     }
@@ -62,8 +92,24 @@ get '/env' => sub {
     return $htmlOutPut;
 };
 
-get '/' => sub {
+get '/req' => sub {
+    addSecurityHeaders();
+    my $hrefReq=request->params;
+    my $requestUri=request->request_uri;
+    my $uriBase=request->uri_base;
+    my $htmlOutPut="<h3>Request</h3>\n<pre>\n";
+    foreach (sort keys %{$hrefReq} ) {
+	$htmlOutPut.=sprintf("%-42s: %s\n","$_","$$hrefReq{$_}");
+    }
+    $htmlOutPut.="Request URI: $requestUri\n";
+    $htmlOutPut.="URI base: $uriBase\n";
+    $htmlOutPut.="\n</pre>\n";
+    return $htmlOutPut;
+};
 
+
+get '/' => sub {
+    addSecurityHeaders();
     my $dbh = get_connection();
 
     eval { $dbh->prepare("SELECT * FROM foo")->execute() };
@@ -99,19 +145,20 @@ post '/' => sub {
 };
 
 get '/health' => sub {
-  my $dbh  = get_connection();
-  my $ping = $dbh->ping();
+    addSecurityHeaders();
+    my $dbh  = get_connection();
+    my $ping = $dbh->ping();
 
-  if ($ping and $ping == 0) {
-    # This is the 'true but zero' case, meaning that ping() is not implemented for this DB type.
-    # See: http://search.cpan.org/~timb/DBI-1.636/DBI.pm#ping
-    return "WARNING: Database health uncertain; this database type does not support ping checks.";
-  }
-  elsif (not $ping) {
-    status 'error';
-    return "ERROR: Database did not respond to ping.";
-  }
-  return "SUCCESS: Database connection appears healthy.";
+    if ($ping and $ping == 0) {
+	# This is the 'true but zero' case, meaning that ping() is not implemented for this DB type.
+	# See: http://search.cpan.org/~timb/DBI-1.636/DBI.pm#ping
+	return "WARNING: Database health uncertain; this database type does not support ping checks.";
+    }
+    elsif (not $ping) {
+	status 'error';
+	return "ERROR: Database did not respond to ping.";
+    }
+    return "SUCCESS: Database connection appears healthy.";
 };
 
 true;
